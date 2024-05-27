4ffde058684a19e9a305d23b5e9455db6f9f311a
sszth <2810688370@qq.com>
2024-05-27 19:14:23 +0800

    1
    1

---
+++ HXLifeSchedule/HXLifeSchedule/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HXLifeSchedule
+//
+//  Created by admin on 2024/5/27.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
